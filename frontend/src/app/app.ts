f39dc519f6e39c912a599d5cc4dc65a8cb071ef7
Rasadhi Sanchala <rasadhi.sanchala@atlato.com>
2025-08-31 13:47:55 +0530

    Implementing basic structure of the frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('task-manager');
 }
